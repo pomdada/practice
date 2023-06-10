7ab13e0f8dc3abee37d6bae1c7815cc63e8a3f04
pomdada <135600158+pomdada@users.noreply.github.com>
2023-06-10 10:58:07 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,22 @@
+public class TangibleAsset
+private String name;
+privete int price;
+prinvate String color;
+
+public TangibleAsset(String name, int price, Stirng color){
+  this.name = name;
+  this.price = price;
+  this.color = color;
+}
+
+public String getName(){
+return this.name;
+}
+
+publid int getPrice(){
+return this.price;
+}
+
+public String getColor(){
+return this.color;
+}
